036994275c991b590d487c5bb2a9d529a22001f6
Tim Streasick <66029458+streatim@users.noreply.github.com>
2024-11-13 13:27:57 -0500

    Module Footer Adjustments
    Comments out code that was already being set in the module's index.php scripts (much like it was in the resources module). When uncommented it broke the footer in Organizations.

--- usage/templates/footer.php
+++ usage/templates/footer.php
@@ -1,6 +1,7 @@
 <?php
 //used to default to previously selected values when back button is pressed
 //if the startWith is defined set it so that it will default to the first letter picked
+/*
 if ((CoralSession::get('res_startWith')) && ($reset != 'Y')){
   echo "startWith = '" . CoralSession::get('res_startWith') . "';";
   echo "$(\"#span_letter_" . CoralSession::get('res_startWith') . "\").removeClass('searchLetter').addClass('searchLetterSelected');";
@@ -17,6 +18,6 @@ if ((CoralSession::get('res_recordsPerPage')) && ($reset != 'Y')){
 if ((CoralSession::get('res_orderBy')) && ($reset != 'Y')){
   echo "orderBy = \"" . CoralSession::get('res_orderBy') . "\";";
 }
-
+*/
 include '../templates/footer.php'; 
 ?>
