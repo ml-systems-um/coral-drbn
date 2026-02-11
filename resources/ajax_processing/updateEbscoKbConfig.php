1194e46b22a75e4cd09263bd3b39e894b0f90790
Tim Streasick <66029458+streatim@users.noreply.github.com>
2026-02-11 13:58:45 -0500

    Remove older form functions
    Removes older form functions that were moved to classes. Currently WIP.

--- resources/ajax_processing/updateEbscoKbConfig.php
+++ resources/ajax_processing/updateEbscoKbConfig.php
@@ -3,8 +3,8 @@
 $enabled = filter_input(INPUT_POST, 'enabled', FILTER_VALIDATE_BOOLEAN);
 $customerId = filter_input(INPUT_POST, 'customerId', FILTER_SANITIZE_STRING);
 $apiKey = filter_input(INPUT_POST, 'apiKey', FILTER_SANITIZE_STRING);
-$ini_file = BASE_DIR . "/admin/configuration.ini";
-require_once BASE_DIR."../common/write_php_ini.php";
+$ini_file = MODULE_DIR . "/admin/configuration.ini";
+require_once MODULE_DIR."../common/write_php_ini.php";
 
 $ini_array = parse_ini_file($ini_file, true);
 
